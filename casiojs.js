f022198db86f3e2968d331e8703e8b171f75a32f
Darshan Tailor <darshanhtailor@gmail.com>
2021-07-29 16:20:30 +0530

    added format change

--- casiojs.js
+++ casiojs.js
@@ -1,7 +1,49 @@
-function giveTime(){
+function giveTime(f) {
     const a = new Date()
-    str = `${a.getHours()}:${a.getMinutes()}:${a.getSeconds()}`;
-    document.getElementById("time").innerHTML = str;
+    if (f == 24) {
+        str = `${a.getHours()}:${a.getMinutes()}:${a.getSeconds()}`;
+        document.getElementById("time").innerHTML = str;
+    }
+    else if (f == 12) {
+        let h = a.getHours();
+        if(h >= 12) ap = "PM";
+        else ap = "AM";
+        if (h > 12) h -= 12;
+        else if (h == 0) h = 12;
+        str = `${h}:${a.getMinutes()}:${a.getSeconds()} ${ap}`;
+        document.getElementById("time").innerHTML = str;
+    }
 }
-setInterval(giveTime, 1000);
+giveTime(24);
+run = setInterval(giveTime, 1000, 24);
+
+let prev = " ";
+function selectForm(id) {
+    if (id != prev) {
+        document.getElementById(id).style.cssText = `
+    background-color: #14ff62;
+    color: black;
+  `;
+        if (id == "f12") {
+            clearInterval(run);
+            prev = "f12";
+            giveTime(12);
+            run = setInterval(giveTime, 1000, 12);
+            document.getElementById("f24").style.cssText = `
+    background-color: black;
+    color: #14ff62;
+  `;
+        }
+        else {
+            clearInterval(run);
+            prev = "f24";
+            giveTime(24);
+            run = setInterval(giveTime, 1000, 24);
+            document.getElementById("f12").style.cssText = `
+    background-color: black;
+    color: #14ff62;
+    `;
+        }
+    }
+}
 
